94a1262ac3997441c1d8cf5f7eca821c5c9c2086
unknown <hnp1@P857543.CAMPUS.nist.gov>
2016-06-23 11:05:34 -0400

    Update 160623

--- src/main/java/gov/nist/pml/hsp/manager/Main.java
+++ src/main/java/gov/nist/pml/hsp/manager/Main.java
@@ -18,10 +18,10 @@ public class Main {
 		
 //		hm.experience2(0,289, base_sb);
 		
-		String result[] = hm.experience(-125*2, 125*2, -125*2, 125*2, base_sb);
+		String result[] = hm.experience(-125*1, 125*1, -125*1, 125*1, base_sb);
 		
-		FileUtils.writeStringToFile(new File("result_2nm_1000nms_r2_100nm_L500nm_STV3%_deflection3.txt"), result[0]);
-		FileUtils.writeStringToFile(new File("result_2nm_1000nms_r2_100nm_L500nm_STV3%_deflection3_TipHeight.txt"), result[1]);
+		FileUtils.writeStringToFile(new File("result_8nm_2000nms_r2_0.2x250nm_L500nm_STV5%_deflection1.5.txt"), result[0]);
+		FileUtils.writeStringToFile(new File("result_8nm_2000nms_r2_0.2x250nm_L500nm_STV5%_deflection1.5_TipHeight.txt"), result[1]);
 	}
 
 }
